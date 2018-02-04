9939b2d683a5537f6d15b888171bc880bdb68238
hoeng <saera111111@gmail.com>
2018-02-04 18:59:53 +0900

    바코드 인식 오픈소스 적용 + 세로모드

---
+++ app/src/main/java/doseo/dodam/com/dodam/AnyOrientationCaptureActivity.java
@@ -0,0 +1,11 @@
+package doseo.dodam.com.dodam;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+
+}
--- app/src/main/java/doseo/dodam/com/dodam/MainActivity.java
+++ app/src/main/java/doseo/dodam/com/dodam/MainActivity.java
@@ -1,7 +1,21 @@
 package doseo.dodam.com.dodam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void scanBarcode(View view) {
+
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+
+        //new IntentIntegrator(this).initiateScan();
+
+        integrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        integrator.setOrientationLocked(false);
+
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("MainActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
