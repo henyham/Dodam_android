ff9d2cb3938e8f6d154f771beaac5584a7735358
조현정\조현정 <guswjd8718@naver.com>
2018-02-12 18:27:42 +0900

    android <-> server connectiong(use httpurlconnection)

--- app/src/main/java/doseo/dodam/com/dodam/MainActivity.java
+++ app/src/main/java/doseo/dodam/com/dodam/MainActivity.java
@@ -3,30 +3,154 @@ package doseo.dodam.com.dodam;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private String tmp_isbn;
+    private TextView textviewJSONText;
+    private String SEARCH_URL = "http://13.125.145.191:8000/test";
+    private String REQUEST_URL = SEARCH_URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonRequestJSON = (Button)findViewById(R.id.button_main_requestjson);
+        textviewJSONText = (TextView)findViewById(R.id.textview_main_jsontext);
+        textviewJSONText.setMovementMethod(new ScrollingMovementMethod());
 
+        buttonRequestJSON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("LOG: ","CLICKED BUTTON");
+                getJSON();
+            }
+        });
+    }
+
+    private final MyHandler mHandler = new MyHandler(this);
+
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> weakReference;
+
+        public MyHandler(MainActivity mainactivity) {
+            weakReference = new WeakReference<MainActivity>(mainactivity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            MainActivity mainactivity = weakReference.get();
+
+            if (mainactivity != null) {
+                switch (msg.what) {
+
+                    case 101:
+
+                        String jsonString = (String)msg.obj;
+                        mainactivity.textviewJSONText.setText(jsonString);
+                        break;
+                }
+            }
+        }
+    }
+
+
+
+
+    public void  getJSON() {
+
+        Thread thread = new Thread(new Runnable() {
+
+            public void run() {
+
+                String result;
+
+                try {
+                    Log.d("LOG: ","getJSON_RUN started");
+                    URL url = new URL(REQUEST_URL);
+                    Log.d("LOG: ",REQUEST_URL);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                    httpURLConnection.setReadTimeout(3000);
+                    httpURLConnection.setConnectTimeout(3000);
+                    //httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.connect();
+
+
+                    int responseStatusCode = httpURLConnection.getResponseCode();
+
+                    InputStream inputStream;
+                    if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                        Log.d("LOG: ","responseStatusCode OK");
+                        inputStream = httpURLConnection.getInputStream();
+                    } else {
+                        Log.d("LOG: ","responseStatusCode NON OK");
+                        Log.d("LOG: ",String.valueOf(responseStatusCode));
+                        inputStream = httpURLConnection.getErrorStream();
+
+                    }
+
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    bufferedReader.close();
+                    httpURLConnection.disconnect();
+
+                    result = sb.toString().trim();
+
+
+                } catch (Exception e) {
+                    result = e.toString();
+                    Log.d("LOG: ",result);
+                }
+
+
+                Message message = mHandler.obtainMessage(101, result);
+                mHandler.sendMessage(message);
+            }
+
+        });
+        thread.start();
     }
 
     public void scanBarcode(View view) {
