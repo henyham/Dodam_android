53cf443c86a31269156a3bed8878d1534c564a19
조현정\조현정 <guswjd8718@naver.com>
2018-03-02 17:49:55 +0900

    add function requestJSON(in PostHttpURLConnection.java)

--- app/src/main/java/doseo/dodam/com/dodam/Connection/PostHttpURLConnection.java
+++ app/src/main/java/doseo/dodam/com/dodam/Connection/PostHttpURLConnection.java
@@ -3,8 +3,13 @@ package doseo.dodam.com.dodam.Connection;
 import android.content.ContentValues;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -101,4 +106,83 @@ public class PostHttpURLConnection {
         }
         return null;
     }
+
+    public String requestJSON(String _url, ContentValues _params){
+        //HttpURLConnection 참조 변수
+        HttpURLConnection urlConn = null;
+        OutputStream os = null;
+        InputStream is = null;
+        ByteArrayOutputStream baos = null;
+
+        /**
+         * 2. HttpURLConnection을 통해 web의 데이터 가져옴
+         */
+        try{
+            URL url = new URL(_url);
+            urlConn = (HttpURLConnection)url.openConnection();
+
+            //[2-1]. urlConn 설정
+            urlConn.setRequestMethod("POST");   //url요청에 대한 메소드 설정
+            urlConn.setRequestProperty("Cache-Control", "no-cache");
+            urlConn.setRequestProperty("Content-Type","application/json");
+            urlConn.setRequestProperty("Accept","application/json");
+            urlConn.setDoOutput(true);
+            urlConn.setDoInput(true);
+
+            //[2-2]. parameter 전달 및 데이터 읽어오기.
+            JSONObject job = new JSONObject();
+
+            //파라미터 키와 값
+            String key;
+            String value;
+
+            try{
+                for(Map.Entry<String, Object> parameter : _params.valueSet()){
+                    key = parameter.getKey();
+                    value = parameter.getValue().toString();
+
+                    //파라미터가 두개 이상일 때 &사용
+                    job.put(key,value);
+
+                }
+            } catch(JSONException e){
+                e.printStackTrace();
+            }
+
+            os = urlConn.getOutputStream();
+            os.write(job.toString().getBytes());  //출력 스트림에 출력
+            os.flush(); // 버퍼링된 모든 출력 바이트 강제 실행
+            os.close(); //출력 스트림을 닫고 모든 시스템 자원 해제
+
+            //[2-3]. 연결 요청 확인
+            //실패시 null을 리턴하고 메서드 종료
+            if(urlConn.getResponseCode() != HttpURLConnection.HTTP_OK)
+                return null;
+
+            //[2-4]. 읽어온 결과물 리턴
+            // 요청한 URL의 출력물을 BufferedReader로 받음
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), "UTF-8"));
+
+            //출력물의 라인과 그 합에 대한 변수
+            String line;
+            String page ="";
+
+            //라인을 받아와 합침.
+            while((line = reader.readLine()) != null){
+                page += line;
+            }
+
+            return page;
+        } catch(MalformedURLException e){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally {
+            if(urlConn != null)
+                urlConn.disconnect();
+        }
+        return null;
+    }
+
+
 }
