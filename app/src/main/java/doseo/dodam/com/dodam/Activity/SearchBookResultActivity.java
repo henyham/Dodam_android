c100264877f264a361f5e1fe8fc78dc4551f2cef
hoeng <saera111111@gmail.com>
2018-03-04 16:39:20 +0900

    changed the way of showing the new result list items (startActivity(intent) -> adapter.notifyDataSetChanged())

--- app/src/main/java/doseo/dodam/com/dodam/Activity/SearchBookResultActivity.java
+++ app/src/main/java/doseo/dodam/com/dodam/Activity/SearchBookResultActivity.java
@@ -52,6 +52,7 @@ public class SearchBookResultActivity extends AppCompatActivity {
     private String REQUEST_URL;
 
     private Boolean lastitemVisibleFlag = false;
+    private Boolean resetListItem = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,17 +65,6 @@ public class SearchBookResultActivity extends AppCompatActivity {
         pageNum = 1;
         searchStr = null;
 
-        //이전 액티비티에서 넘어온 데이터를 받음
-        Intent intentData = getIntent();
-
-        //데이터 저장
-        if(intentData.getExtras() != null) {
-            jsonData = intentData.getExtras().getString("resultObject");    // JSONArrary(String 형) 값 content에 저장
-            pageNum = intentData.getExtras().getInt("pageNum");             // 전 액티비티에서 검색했을 때 사용한 page 값
-            searchStr = intentData.getExtras().getString("searchStr");      // 전 액티비티에서 검색한 책의 제목
-        }
-        Log.d("progressTag","intent data: " + jsonData);
-
         //setting button, edit text, listview
         bookSearchBtn = findViewById(R.id.book_search_btn);
         bookSearchBar = findViewById(R.id.book_search_bar);
@@ -82,18 +72,6 @@ public class SearchBookResultActivity extends AppCompatActivity {
 
         //리스트 생성
         resultList = new ArrayList<String>();
-
-        //검색에 사용할 데이터를 미리 저장
-        if(jsonData != null){
-            try {
-                JSONObject obj = new JSONObject(jsonData);
-                Log.d("progressTag", "before settinfList: " + obj.getJSONArray("documents").toString());
-                //검색 결과 리스트 나열
-                settingList(obj);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
 
         //리스트의 모든 데이터를 arraylist에 복사(result list의 복사본 생성)
         arrayList = new ArrayList<String>();
@@ -104,6 +82,8 @@ public class SearchBookResultActivity extends AppCompatActivity {
         //리스트뷰에 어댑터를 연결
         resultListView.setAdapter(adapter);
 
+
+        //무한 스크롤
         resultListView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -128,6 +108,7 @@ public class SearchBookResultActivity extends AppCompatActivity {
                 }
             }
 
+            //스크롤의 끝에 도달했는지 판별
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
                 lastitemVisibleFlag = (totalItemCount > 0) && (firstVisibleItem + visibleItemCount >= totalItemCount);
@@ -147,6 +128,8 @@ public class SearchBookResultActivity extends AppCompatActivity {
         //URL 설정(제목으로 검색)
         REQUEST_URL = "https://dapi.kakao.com/v2/search/book?target=title&size=20&query=" + text;
         pageNum = 1;
+        searchStr = bookSearchBar.getText().toString();
+        resetListItem = true;
         //Log.d("progressTag", "string of REQUEST_URL: " + REQUEST_URL);
 
         SearchBookByWord searchBookByWord = new SearchBookByWord(REQUEST_URL,"Authorization", "KakaoAK " + getResources().getString(R.string.kakao_app_rest_key),null);
@@ -191,13 +174,14 @@ public class SearchBookResultActivity extends AppCompatActivity {
 
             //doInBackground로부터 리턴된 값이 onPostExecute의 매개변수
             //json에서 getString, getInt 등으로 필요한 정보 빼낸다.
+
             JSONObject tmpJson;
             String sendData ="";
+
             if(jsonData != null){
                 try {
                     tmpJson = new JSONObject(jsonData);
-                    Log.d("progressTag", "result JSON Object: "+j.toString());
-                    Log.d("progressTag", "result JSON Object length: " + j.length());
+
                     for(int i=0; i<j.getJSONArray("documents").length();i++){
                         try {
                             tmpJson.getJSONArray("documents").put(j.getJSONArray("documents").getJSONObject(i));
@@ -209,25 +193,19 @@ public class SearchBookResultActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-            } else {
+            }
+            else {
                 sendData = j.toString();
             }
-            Intent intent = new Intent(SearchBookResultActivity.this, SearchBookResultActivity.class);
 
-            intent.putExtra("resultObject", sendData);
-            intent.putExtra("pageNum", pageNum);
-            if(searchStr != null){
-                intent.putExtra("searchStr", searchStr);
-            } else {
-                intent.putExtra("searchStr", bookSearchBar.getText().toString());
+            jsonData = sendData;
+
+            try {
+                settingList(j);
+                resetListItem = false;
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-
-            Log.d("progressTag", "***in Post Execute: resultObj = " + sendData);
-            Log.d("progressTag", "***in Post Execute: pageNum = " + pageNum);
-            Log.d("progressTag", "***in Post Execute: searchStr = " + searchStr);
-
-            startActivity(intent);
-            finish();
 
         }
 
@@ -235,71 +213,18 @@ public class SearchBookResultActivity extends AppCompatActivity {
 
     //검색에 사용될 데이터를 리스트에 추가한다다
     private void settingList(JSONObject jsonObject) throws JSONException {
-        //Log.d("progressTag", "in settingList");
-        //Log.d("progressTag", "from settingList" + jsonObject.getJSONArray("documents").getJSONObject(0).getString("title"));
-
+        if(resetListItem == true){
+            resultList.clear();
+        }
         JSONArray returnResult = jsonObject.getJSONArray("documents");
-        Log.d("progressTag", " result JSONArrary: " + returnResult.toString());
-        Log.d("progressTag", String.valueOf(returnResult.length()));
 
             for(int i=0;i<returnResult.length();i++){
-                Log.d("titleListTag", String.valueOf(i));
-                Log.d("titleListTag", returnResult.getJSONObject(i).toString());
-                //returnResult.getJSONObject(i);
-                Log.d("titleListTag",  returnResult.getJSONObject(i).getString("title"));
                 resultList.add(returnResult.getJSONObject(i).getString("title").toString());
         }
-    }
 
-    /*
-    private Runnable updateUI = new Runnable() {
-        public void run() {
-            SearchBookResultActivity.this.adapter.notifyDataSetChanged();
-        }
-    };
-    */
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //검색을 수행하는 메소드
-    /*
-    public void search(String charText){
-
-        //문자 입력시마다 리스트르르 지우고 새로 뿌려준다
-        resultList.clear();
-
-        // 문자 입력이 없을때는 모든 데이터를 보여준다.
-        if (charText.length() == 0) {
-            resultList.addAll(arrayList);
-        }
-        // 문자 입력을 할때..
-        else
-        {
-            // 리스트의 모든 데이터를 검색한다.
-            for(int i = 0;i < arrayList.size(); i++)
-            {
-                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
-                if (arrayList.get(i).toLowerCase().contains(charText))
-                {
-                    // 검색된 데이터를 리스트에 추가한다.
-                    resultList.add(arrayList.get(i));
-                }
-            }
-        }
         // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
         adapter.notifyDataSetChanged();
     }
-    */
 
 
 }
